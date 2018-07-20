138b0eff31b8b911687fea4612fedb9f74e4c227
Donovan Preston <donovanpreston@gmail.com>
2018-07-20 12:48:42 -0400

    Add a selection box on mousemove

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 
+(function (){
 let getAbsoluteUrl = (function() {
 	let a;
 
@@ -66,17 +67,66 @@ function serializeDOM(node) {
     return Array.from(generateSerializedDOMParts(node)).join('');
 }
 
+function scroll(event) {
+    if (overlay) overlay.remove();
+    let highlight = document.getElementById('highlight-box');
+    if (highlight) highlight.remove();
+}
+
+function mousemove(event) {
+    const x = event.clientX;
+    const y = event.clientY;
+    let node = document.getElementById('highlight-box');
+    if (node) node.remove();
+    overlay.remove();
+    let el = document.elementFromPoint(x, y);
+    document.body.appendChild(overlay);
+    if (node) { document.body.appendChild(node); }
+    if (!node) {
+        node = document.createElement('div');
+        node.id = 'highlight-box';
+        node.style.position = 'fixed';
+        node.style.border = '1px dashed black';
+        node.style.borderImage = `url('${browser.extension.getURL("icons/selection.gif")}') 1 1 1 1 repeat repeat`;
+        document.body.appendChild(node);
+    }
+    const bounds = el.getBoundingClientRect();
+    node.style.top = bounds.top;
+    node.style.left = bounds.left;
+    node.style.height = bounds.height;
+    node.style.width = bounds.width;
+}
+
 function click(event) {
     event.preventDefault();
-    const x = event.pageX + window.scrollX - window.pageXOffset;
-    const y = event.pageY + window.scrollY - window.pageYOffset
+    if (overlay) overlay.remove();
+    let highlight = document.getElementById('highlight-box');
+    if (highlight) highlight.remove();
+    const x = event.clientX;
+    const y = event.clientY;
     const el = document.elementFromPoint(x, y);
     const serialized = serializeDOM(el);
     //console.log(serialized);
-    document.removeEventListener("click", click, false);
     browser.runtime.sendMessage({"addScrap": serialized});
+    document.removeEventListener("click", click, false);
+    document.removeEventListener("mousemove", mousemove, false);
+    window.removeEventListener("scroll", scroll, false);
 }
 
-document.addEventListener("click", click, false);
+let overlay = document.getElementById('scrapbook-highlight-overlay');
+if (!overlay) {
+    overlay = document.createElement('div');
+    overlay.style.postion = 'fixed';
+    overlay.style.top = '0';
+    overlay.style.left = '0';
+    overlay.style.bottom = '0';
+    overlay.style.right = '0';
+    overlay.style.backgroundColor = 'rgba(1,1,1,0.5)';
+    document.body.appendChild(overlay);
+    document.addEventListener("click", click, false);
+    document.addEventListener("mousemove", mousemove, false);
+    window.addEventListener("scroll", scroll, false);
+}
 
+})();
 
